b9b6eab87ef57706217658b6f0ec015e41e7daa4
kvrancic <kvrancic11@gmail.com>
2025-04-25 21:18:33 +0200

    initial setup

--- components/hero.tsx
+++ components/hero.tsx
@@ -17,9 +17,9 @@ export default function Header() {
           <NextLogo />
         </a>
       </div>
-      <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
+      <h1 className="sr-only">Pederi Pedercineeeeee Ne mogu vjerovat da niste dosli</h1>
       <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        The fastest way to build apps with{" "}
+      Pederi Pedercineeeeee Ne mogu vjerovat da niste dosli
         <a
           href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
           target="_blank"
